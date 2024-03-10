ef007972640f73bcd09de06669c57f375ed97bcc
raditbirk <162890799+raditbirk@users.noreply.github.com>
2024-03-10 19:22:38 +0000

    Pending changes exported from your codespace

--- standard-integration/server/server.js
+++ standard-integration/server/server.js
@@ -59,7 +59,7 @@ const createOrder = async (cart) => {
       {
         amount: {
           currency_code: "USD",
-          value: "110.00",
+          value: "100.00",
         },
       },
     ],
